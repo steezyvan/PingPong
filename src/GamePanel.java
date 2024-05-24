395ceb7da3876907bc305c9a09ca5f3c5f637f84
steezyvan <91402817+steezyvan@users.noreply.github.com>
2024-05-24 14:23:01 -0700

    First Commit

---
+++ src/GamePanel.java
@@ -0,0 +1,48 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class GamePanel {
+    GamePanel(){
+
+    }
+
+    public void newball(){
+
+    }
+
+    public void newPaddles(){
+
+    }
+
+    public void paint(Graphics G){
+
+    }
+
+    public void draw(Graphics G){
+
+    }
+
+    public void move(){
+
+    }
+
+    public void checkCollision(){
+
+    }
+
+    public void run(){
+
+    }
+
+    public class AL extends KeyAdapter{
+        public void KeyPressed(KeyEvent e){
+
+        }
+
+        public void KeyReleased(KeyEvent e){
+
+        }
+    }
+}
